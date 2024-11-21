d91db41ea932cbd5a77a15bbd8c28baae84f78a0
Daria Karataeva <daria.karataeva@yandex.ru>
2024-11-21 15:15:54 +0300

    fix file name 2

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -116,13 +116,13 @@ describe("pageLoader", () => {
     });
 
     const img1 =
-      "/home/runner/work/backend-project-4/backend-project-4/ru-hexlet-io-courses_files/ru-hexlet-io-courses-assets-nodejs-png.png";
+      "/home/runner/work/backend-project-4/backend-project-4/ru-hexlet-io-courses_files/ru-hexlet-io-courses-assets-nodejs.png";
     const img2 =
-      "/home/runner/work/backend-project-4/backend-project-4/ru-hexlet-io-courses_files/ru-hexlet-io-photos-react-png.png";
+      "/home/runner/work/backend-project-4/backend-project-4/ru-hexlet-io-courses_files/ru-hexlet-io-photos-react.png";
     const css =
-      "/home/runner/work/backend-project-4/backend-project-4/ru-hexlet-io-courses_files/ru-hexlet-io-courses-assets-application-css.css";
+      "/home/runner/work/backend-project-4/backend-project-4/ru-hexlet-io-courses_files/ru-hexlet-io-courses-assets-application.css";
     const js =
-      "/home/runner/work/backend-project-4/backend-project-4/ru-hexlet-io-courses_files/ru-hexlet-io-courses-assets-scripts-js.js";
+      "/home/runner/work/backend-project-4/backend-project-4/ru-hexlet-io-courses_files/ru-hexlet-io-courses-assets-scripts.js";
 
     expect(fileResult.indexOf(img1)).toBeGreaterThanOrEqual(0);
     expect(fileResult.indexOf(img2)).toBeGreaterThanOrEqual(0);
--- src/page-loader.js
+++ src/page-loader.js
@@ -12,7 +12,7 @@ const getFileName = (srcPath, pagePath) => {
   const src = srcPath.startsWith("/")
   ? `${origin}${srcPath}`
   : srcPath;
-  const srcName = src.replace(/(https|http):\/\//g, '').replace(/\W+/g, "-");
+  const srcName = src.replace(/(https|http):\/\//g, '').replace(/.[a-zA-Z]*$/, '').replace(/\W+/g, "-");
   return `${srcName}${path.parse(src).ext}`
 }
 
