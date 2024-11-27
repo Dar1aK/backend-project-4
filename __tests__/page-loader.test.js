2ff1c4a4e9b0fae22b3c8ee974b9a76f6a055fe4
Daria Karataeva <daria.karataeva@yandex.ru>
2024-11-27 18:28:58 +0300

    fix eslint 5

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -9,71 +9,42 @@ describe('pageLoader', () => {
     nock('https://site.com')
       .persist()
       .get('/blog/about')
-      .reply(200, () => {return fsp.readFile(
-          path.resolve(
-            __dirname,
-            '../__fixtures__/blog/about/site-com-blog-about.html',
-          ),
-          { encoding: 'utf8' },
-        );},
-      )
+      .reply(200, () => fsp.readFile(path.resolve(__dirname,
+            '../__fixtures__/blog/about/site-com-blog-about.html',),
+          { encoding: 'utf8' },),)
       .get('/photos/me.jpg')
-      .reply(200, () => {return fsp.readFile(path.resolve(__dirname, '../__fixtures__/photos/me.jpg'), {
+      .reply(200, () => fsp.readFile(path.resolve(__dirname, '../__fixtures__/photos/me.jpg'), {
           encoding: 'binary',
-        });},
-      )
+        }),)
       .get('/blog/about/assets/styles.css')
-      .reply(200, () => {return fsp.readFile(
-          path.resolve(
-            __dirname,
-            '../__fixtures__/blog/about/assets/styles.css',
-          ),
-          { encoding: 'binary' },
-        );},
-      )
+      .reply(200, () => fsp.readFile(path.resolve(__dirname,
+            '../__fixtures__/blog/about/assets/styles.css',),
+          { encoding: 'binary' },),)
       .get('/assets/scripts.js')
-      .reply(200, () => {return fsp.readFile(
-          path.resolve(__dirname, '../__fixtures__/assets/scripts.js'),
-          { encoding: 'binary' },
-        );},
-      )
+      .reply(200, () => fsp.readFile(path.resolve(__dirname, '../__fixtures__/assets/scripts.js'),
+          { encoding: 'binary' },),)
       .get('/11111')
-      .reply(200, () => {return fsp.readFile(path.resolve(__dirname, '../__fixtures__/11111'));},
-      )
+      .reply(200, () => fsp.readFile(path.resolve(__dirname, '../__fixtures__/11111')),)
       .get('/courses/assets/not-exist.css')
       .reply(404);
 
     nock('http://localhost')
       .persist()
       .get('/blog/about')
-      .reply(200, () => {return fsp.readFile(
-          path.resolve(
-            __dirname,
-            '../__fixtures__/blog/about/localhost-blog-about.html',
-          ),
-          { encoding: 'utf8' },
-        );},
-      )
+      .reply(200, () => fsp.readFile(path.resolve(__dirname,
+            '../__fixtures__/blog/about/localhost-blog-about.html',),
+          { encoding: 'utf8' },),)
       .get('/photos/me.jpg')
-      .reply(200, () => {return fsp.readFile(path.resolve(__dirname, '../__fixtures__/photos/me.jpg'), {
+      .reply(200, () => fsp.readFile(path.resolve(__dirname, '../__fixtures__/photos/me.jpg'), {
           encoding: 'binary',
-        });},
-      )
+        }),)
       .get('/blog/about/assets/styles.css')
-      .reply(200, () => {return fsp.readFile(
-          path.resolve(
-            __dirname,
-            '../__fixtures__/blog/about/assets/styles.css',
-          ),
-          { encoding: 'binary' },
-        );},
-      )
+      .reply(200, () => fsp.readFile(path.resolve(__dirname,
+            '../__fixtures__/blog/about/assets/styles.css',),
+          { encoding: 'binary' },),)
       .get('/assets/scripts.js')
-      .reply(200, () => {return fsp.readFile(
-          path.resolve(__dirname, '../__fixtures__/assets/scripts.js'),
-          { encoding: 'binary' },
-        );},
-      );
+      .reply(200, () => fsp.readFile(path.resolve(__dirname, '../__fixtures__/assets/scripts.js'),
+          { encoding: 'binary' },),);
   });
 
   test('run pageLoader', async () => {
@@ -82,17 +53,11 @@ describe('pageLoader', () => {
     const fileResult = await fsp.readFile(path.resolve(htmlPath), {
       encoding: 'utf8',
     });
-    const fixture = await fsp.readFile(
-      path.resolve(
-        __dirname,
-        '../__fixtures__/result/site-com-blog-about.html',
-      ),
-      { encoding: 'utf8' },
-    );
+    const fixture = await fsp.readFile(path.resolve(__dirname,
+        '../__fixtures__/result/site-com-blog-about.html',),
+      { encoding: 'utf8' },);
 
-    expect(JSON.stringify(fileResult).replace(/\s+/g, '')).toBe(
-      JSON.stringify(fixture).replace(/\s+/g, ''),
-    );
+    expect(JSON.stringify(fileResult).replace(/\s+/g, '')).toBe(JSON.stringify(fixture).replace(/\s+/g, ''),);
   });
 
   test('run pageLoader on localhost', async () => {
@@ -101,17 +66,11 @@ describe('pageLoader', () => {
     const fileResult = await fsp.readFile(path.resolve(htmlPath), {
       encoding: 'utf8',
     });
-    const fixture = await fsp.readFile(
-      path.resolve(
-        __dirname,
-        '../__fixtures__/result/localhost-blog-about.html',
-      ),
-      { encoding: 'utf8' },
-    );
+    const fixture = await fsp.readFile(path.resolve(__dirname,
+        '../__fixtures__/result/localhost-blog-about.html',),
+      { encoding: 'utf8' },);
 
-    expect(JSON.stringify(fileResult).replace(/\s+/g, '')).toBe(
-      JSON.stringify(fixture).replace(/\s+/g, ''),
-    );
+    expect(JSON.stringify(fileResult).replace(/\s+/g, '')).toBe(JSON.stringify(fixture).replace(/\s+/g, ''),);
   });
 
   test('check pageLoader with sources', async () => {
@@ -134,14 +93,10 @@ describe('pageLoader', () => {
   });
 
   test('check pageLoader with error link', async () => {
-    expect(
-      async () => {return await pageLoader('https://site.com/11111');},
-    ).rejects.toThrow();
+    expect(async () => await pageLoader('https://site.com/11111'),).rejects.toThrow();
   });
 
   test('run pageLoader with not existing directory', async () => {
-    expect(
-      async () => {return await pageLoader('https://site.com/blog/about', '/not-exist');},
-    ).rejects.toThrow();
+    expect(async () => await pageLoader('https://site.com/blog/about', '/not-exist'),).rejects.toThrow();
   });
 });
